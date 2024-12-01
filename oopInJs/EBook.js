258736aa50da92849be8f6935bf2d8804c8cc97b
Sasha Bezdolia <bezdolya.aleksandr@gmail.com>
2024-12-01 21:27:15 +0200

    homework with some previous extra tasks

---
+++ oopInJs/EBook.js
@@ -0,0 +1,28 @@
+import { Book } from "./Book.js";
+export class EBook extends Book {
+
+  constructor(name, author, releaseYear, fileFormat) {
+    super(name, author, releaseYear);
+    this._fileFormat = fileFormat;
+  }
+
+  printInfo() {
+    console.log(`The EBook "${this._name}" ${this._author} was released in ${this._releaseYear} and has a format "${this._fileFormat}".`);
+  }
+
+  set fileFormat(fileFormat) {
+    if(fileFormat.length > 4) {
+      console.log('Wrong file format');
+    } else {
+      this._fileFormat = fileFormat;
+    }
+  }
+
+  get fileFormat(){
+    return this._fileFormat;
+  }
+
+  static bookToEBook (book, _fileFormat) {
+    return { ...book, _fileFormat};
+  }
+}
---
+++ oopInJs/task1.js
@@ -0,0 +1,17 @@
+import { Book } from "./Book.js";
+import { EBook } from "./EBook.js";
+
+const heroes = new Book ('Heroes', 'Joe Abercrombie', 2011);
+const potter2 = new Book ('Harry Potter and the Chamber of Secrets', 'J.K. Rowling', 1999);
+const winds = new Book ('Winds of winter', 'George R.R. Martin', 2077);
+const redCountryEbook = new EBook ('Red Country', 'Joe Abercrombie', 2012, 'mobi');
+
+heroes.printInfo();
+potter2.printInfo();
+winds.printInfo();
+redCountryEbook.printInfo();
+
+console.log('-------');
+console.log(Book.oldestBook([heroes, potter2, winds, redCountryEbook]));
+console.log('-------');
+console.log(EBook.bookToEBook(heroes, 'pdf'));
